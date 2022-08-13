217b3b0de173369b41f3dc6ea0241f60381d0307
SiriusAshling <53594055+SiriusAshling@users.noreply.github.com>
2022-08-13 15:12:17 +0200

    change annotations format

--- src/headers.rs
+++ src/headers.rs
@@ -1,29 +1,33 @@
 use wasm_bindgen::prelude::*;
 
-use num_enum::TryFromPrimitive;
-
 use wotw_seedgen::header::Header;
 use wotw_seedgen::header::HeaderDocumentation as SeedgenHeaderDocumentation;
 use wotw_seedgen::header::ParameterInfo as SeedgenParameterInfo;
 use wotw_seedgen::header::ParameterDefault as SeedgenParameterDefault;
+use wotw_seedgen::header::Annotation as SeedgenAnnotation;
 
 use wasm_bindgen_helper_macros::*;
 
-ts_enum! {
-    #[wasm_bindgen]
-    #[derive(TryFromPrimitive)]
-    #[repr(u8)]
-    /// Annotations providing meta information about how to treat the header
-    pub enum Annotation {
-        /// Hide this header from the user, it is only to be used internally through includes
-        Hide,
-    }
+/// Meta information contained in a header's annotations
+#[wasm_bindgen]
+#[derive(Default)]
+pub struct Annotations {
+    /// Hide this header from the user, it is only to be used internally through includes
+    pub hide: bool,
+    /// Put this header into a category with other, similar headers
+    #[wasm_bindgen(getter_with_clone)]
+    pub category: Option<String>,
 }
-
-wrapper_list! {
-    #[wasm_bindgen]
-    pub struct __AnnotationList {
-        inner: IntoIter<Annotation>,
+impl From<Vec<SeedgenAnnotation>> for Annotations {
+    fn from(annotation_vec: Vec<SeedgenAnnotation>) -> Annotations {
+        let mut annotations = Annotations::default();
+        for annotation in annotation_vec {
+            match annotation {
+                SeedgenAnnotation::Hide => annotations.hide = true,
+                SeedgenAnnotation::Category(category) => annotations.category = Some(category),
+            }
+        }
+        annotations
     }
 }
 
@@ -31,13 +35,8 @@ wrapper_list! {
 /// 
 /// This will only parse the minimum amount required to know the annotations
 #[wasm_bindgen]
-pub fn parse_annotations(header: &str) -> Result<AnnotationArray, String> {
-    let annotations = Header::parse_annotations(header)?
-        .into_iter()
-        .map(|annotation| (annotation as u8).try_into().unwrap())
-        .collect::<Vec<_>>();
-
-    Ok(__AnnotationList::from(annotations).into_js_array())
+pub fn parse_annotations(header: &str) -> Result<Annotations, String> {
+    Header::parse_annotations(header).map(Annotations::from)
 }
 
 #[wasm_bindgen]
@@ -79,18 +78,11 @@ wrapper_list! {
 pub struct Parameter {
     #[wasm_bindgen(getter_with_clone)]
     pub identifier: String,
-    parameter_type: ParameterType,
+    pub parameter_type: ParameterType,
     #[wasm_bindgen(getter_with_clone)]
     pub default_value: String,
     #[wasm_bindgen(getter_with_clone)]
     pub documentation: Option<String>,
-}
-#[wasm_bindgen]
-impl Parameter {
-    #[wasm_bindgen(getter, js_name = "type")]
-    pub fn parameter_type(&self) -> ParameterTypeEnum {
-        self.parameter_type.into_js_enum()
-    }
 }
 
 impl From<SeedgenParameterInfo> for Parameter {
@@ -102,15 +94,13 @@ impl From<SeedgenParameterInfo> for Parameter {
     }
 }
 
-ts_enum! {
-    #[wasm_bindgen]
-    #[derive(Clone, Copy)]
-    pub enum ParameterType {
-        Bool,
-        Int,
-        Float,
-        String,
-    }
+#[wasm_bindgen]
+#[derive(Clone, Copy)]
+pub enum ParameterType {
+    Bool,
+    Int,
+    Float,
+    String,
 }
 
 impl From<SeedgenParameterDefault> for ParameterType {
